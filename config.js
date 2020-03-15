d631378b6bd92cc61f2b573005fb4ef9f1849731
wild <240440056@qq.com>
2020-03-15 14:50:06 +0800

    note

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = { port: 8101, target: 'https://api.binance.com' };
-// const config = { port: 8102, target: 'https://api.binance.com' });
+// const config = { port: 8102, target: 'https://www.bitmex.com' });
 
 module.exports = {
   config
