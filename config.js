912575dc11598d960f786227e811b95318568654
lx2720 <13683050032@163.com>
2023-05-06 17:46:04 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,7 @@ const port = 8101;
 
 const targetMap = new Map();
 targetMap.set('binance', 'https://api.binance.com');
+targetMap.set('binancef', 'https://fapi.binance.com');
 targetMap.set('bitmex', 'https://www.bitmex.com');
 targetMap.set('huobi', 'https://api.hbdm.com');
 
